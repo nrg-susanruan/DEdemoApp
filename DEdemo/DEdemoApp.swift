9f1396f63aa746e06aa9d1a2973f786f51f9ecbb
SusanRuan <92039552+SusanRuan@users.noreply.github.com>
2024-12-04 15:52:42 -0500

    Initial Commit

---
+++ DEdemo/DEdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEdemoApp.swift
+//  DEdemo
+//
+//  Created by susan ruan on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DEdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DEdemoTests/DEdemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  DEdemoTests.swift
+//  DEdemoTests
+//
+//  Created by susan ruan on 12/4/24.
+//
+
+import Testing
+
+struct DEdemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
